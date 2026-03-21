344af254e792cf8064fe0d50a82f28debbb67039
huizecavia <bomaltennishal@outlook.com>
2026-03-21 12:11:51 +0100

    hike rest

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +10,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('Legend_Angular');
+
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef)
+
+  ngOnInit() {
+    const subscription = this.httpClient.get('http://localhost:8080/hike').subscribe({
+      next: (resData) => {
+        console.log(resData);
+      }
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+
+    })
+  }
 }
